ad9a35eba7db660a9d5cf4a99f8fc2d28cc3f741
nethm <it21160998@my.sliit.lk>
2023-05-14 16:38:18 +0530

    1st

--- app/src/main/java/com/example/myapplication/OrganizationModel.kt
+++ app/src/main/java/com/example/myapplication/OrganizationModel.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
 data class OrganizationModel(
-    var orggId = null,
+    var orggId : String? =null,
     var orgName:String?= null,
     var orgId:String?= null,
     var orgEmail:String?= null,
